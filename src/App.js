8ab5dda2798b5f654470bb47f3403aa73e61ea89
ltw11220 <61130243+ltw11220@users.noreply.github.com>
2021-12-30 20:14:26 -0500

    update3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import SortingVisualizer from './SortingVisualizer/SortingVisualizer';
 
 function App() {
   return (
     <div className="App">
+      <img src = {logo} className = 'App-logo' alt = 'logo'></img>
       <SortingVisualizer></SortingVisualizer>
     </div>
   );
--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -27,13 +27,11 @@ export default class SortingVisualizer extends React.Component{
     
 
         return (
-        <>
+        <div className='array-container'>
                 {array.map((value, idx) => (
-                    <div className='array-bar' key={idx}>
-                        {value}
-                    </div>
+                    <div className='array-bar' key={idx} style = {{height: '${value}px'}}></div>
                 ))}
-        </>
+        </div>
         );
     }
 }
